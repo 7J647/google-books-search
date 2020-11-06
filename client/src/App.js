36410c20c6e9ed37b4122112a0550e9a1725954d
7J647 <007J647@gmail.com>
2020-11-05 22:23:15 -0500

    Jumbotron working

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
-// import { useEffect } from "react";
-import Search from "./pages/Search";
+import React, { useState } from "react";
+import Jumbotron from "./components/Jumbotron";
+// import Nav from "./components/Nav";
+// import Input from "./components/Input";
+// import Button from "./components/Button";
+// import API from "./utils/API";
+// import { RecipeList, RecipeListItem } from "./components/RecipeList";
+// import { Container, Row, Col } from "./components/Grid";
 
-import "./App.css";
+// import { useEffect } from "react";
+// import Search from "./pages/Search";
+
+// import "./App.css";
 // import axios from "axios";
 
 function App() {
   return (
     <div>
-      <Search />
+      <Jumbotron />
       
     </div>
   );
