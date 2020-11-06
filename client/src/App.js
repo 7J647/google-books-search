f30d6d175b3190b4ef8d376fdbb928ef3265b805
7J647 <007J647@gmail.com>
2020-11-05 22:44:04 -0500

    search bar appears

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import Jumbotron from "./components/Jumbotron";
 // import Nav from "./components/Nav";
-// import Input from "./components/Input";
-// import Button from "./components/Button";
+import Input from "./components/Input";
+import Button from "./components/Button";
 // import API from "./utils/API";
 // import { RecipeList, RecipeListItem } from "./components/RecipeList";
-// import { Container, Row, Col } from "./components/Grid";
+import { Container, Row, Col } from "./components/Grid";
 
 // import { useEffect } from "react";
 // import Search from "./pages/Search";
@@ -17,6 +17,35 @@ function App() {
   return (
     <div>
       <Jumbotron />
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <form>
+              <Container>
+                <Row>
+                  <Col size="xs-9 sm-10">
+                    <Input
+                      name="RecipeSearch"
+                      // value={recipeSearch}
+                      // onChange={handleInputChange}
+                      placeholder="Search For a Book"
+                    />
+                  </Col>
+                  <Col size="xs-3 sm-2">
+                    <Button
+                      // onClick={handleFormSubmit}
+                      type="success"
+                      className="input-lg"
+                    >
+                        Search
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </form>
+          </Col>
+        </Row>
+        </Container>
       
     </div>
   );
