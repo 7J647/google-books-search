506a037978b5234659713f28fa4913d77f868346
7J647 <007J647@gmail.com>
2020-11-07 19:12:51 -0500

    used Router and Switch on App.js to try and use two different pages, one for search which is also '/' and one for saved

--- client/src/App.js
+++ client/src/App.js
@@ -1,104 +1,138 @@
-import React, { useState } from "react";
-import Jumbotron from "./components/Jumbotron";
-import Nav from "./components/Nav";
-import Input from "./components/Input";
-import Button from "./components/Button";
-import API from "./utils/API";
-import { BookList, BookListItem } from "./components/BookList";
-import { Container, Row, Col } from "./components/Grid";
+// import React, { useState } from "react";
+// import Search from "./pages/Search";
+// import Jumbotron from "./components/Jumbotron";
+// import Nav from "./components/Nav";
+// import Input from "./components/Input";
+// import Button from "./components/Button";
+// import API from "./utils/API";
+// import { BookList, BookListItem } from "./components/BookList";
+// import { Container, Row, Col } from "./components/Grid";
+
+// import { useEffect } from "react";
+// // import Search from "./pages/Search";
+// // import API from "./utils/API";
+
+// // import "./App.css";
+// // import axios from "axios";
+
+// function App() {
+
+//   const [books, setBooks] = useState([]);
+//   const [bookSearch, setBookSearch] = useState("");
+
+//   useEffect(()=>{
+//     API.searchTerms("Ian Fleming").then(response=>
+//       console.log(response.data))
+//   },[])
+
+//   const handleInputChange = event => {
+//     // Destructure the name and value properties off of event.target
+//     // Update the appropriate state
+//     const { value } = event.target;
+//     setBookSearch(value);
+//   };
+
+//   const handleFormSubmit = event => {
+//     // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+//     event.preventDefault();
+//     API.searchTerms(bookSearch)
+//       .then(res => setBooks(res.data.items))
+//       .catch(err => console.log(err));
+//   };
+
+
+//   return (
+//     <div>
+//      <Nav/>
+//       <Jumbotron />
+//       <Container>
+//         <Row>
+//           <Col size="md-12">
+//             <form>
+//               <Container>
+//                 <Row>
+//                   <Col size="xs-9 sm-10">
+//                     <Input
+//                       name="RecipeSearch"
+//                       value={bookSearch}
+//                       onChange={handleInputChange}
+//                       placeholder="Search For a Book"
+//                     />
+//                   </Col>
+//                   <Col size="xs-3 sm-2">
+//                     <Button
+//                       onClick={handleFormSubmit}
+//                       type="success"
+//                       className="input-lg"
+//                     >
+//                         Search
+//                     </Button>
+//                   </Col>
+//                 </Row>
+//               </Container>
+//             </form>
+//           </Col>
+//         </Row>
+//         <Row>
+//           <Col size="xs-12">
+//             {!books.length ? (
+//               <h1 className="text-center">No Books to Display</h1>
+//             ) : (
+//               <BookList>
+//                 {books.map(book => {
+//                   return (
+//                     <BookListItem
+//                       key={book.volumeInfo.title}
+//                       title={book.volumeInfo.title}
+//                       authors={book.volumeInfo.authors}
+//                       description={book.volumeInfo.description}
+//                       link={book.volumeInfo.infoLink}
+//                       thumbnail={book.volumeInfo.imageLinks.thumbnail}
+//                     />
+//                   );
+//                 })}
+//               </BookList>
+//             )}
+//           </Col>
+//         </Row>
+//         </Container>
+      
+//     </div>
+//   );
+// }
+
+// export default App;
 
 import { useEffect } from "react";
-// import Search from "./pages/Search";
-// import API from "./utils/API";
-
-// import "./App.css";
-// import axios from "axios";
+import Search from "./pages/Search";
+// import Saved from "./pages/Saved";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 
 function App() {
 
-  const [books, setBooks] = useState([]);
-  const [bookSearch, setBookSearch] = useState("");
-
-  useEffect(()=>{
-    API.searchTerms("Ian Fleming").then(response=>
-      console.log(response.data))
-  },[])
-
-  const handleInputChange = event => {
-    // Destructure the name and value properties off of event.target
-    // Update the appropriate state
-    const { value } = event.target;
-    setBookSearch(value);
-  };
-
-  const handleFormSubmit = event => {
-    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
-    event.preventDefault();
-    API.searchTerms(bookSearch)
-      .then(res => setBooks(res.data.items))
-      .catch(err => console.log(err));
-  };
-
-
+  useEffect(() => {
+    console.log("Make an API call");
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
+<Router>
     <div>
-     <Nav/>
-      <Jumbotron />
-      <Container>
-        <Row>
-          <Col size="md-12">
-            <form>
-              <Container>
-                <Row>
-                  <Col size="xs-9 sm-10">
-                    <Input
-                      name="RecipeSearch"
-                      value={bookSearch}
-                      onChange={handleInputChange}
-                      placeholder="Search For a Book"
-                    />
-                  </Col>
-                  <Col size="xs-3 sm-2">
-                    <Button
-                      onClick={handleFormSubmit}
-                      type="success"
-                      className="input-lg"
-                    >
-                        Search
-                    </Button>
-                  </Col>
-                </Row>
-              </Container>
-            </form>
-          </Col>
-        </Row>
-        <Row>
-          <Col size="xs-12">
-            {!books.length ? (
-              <h1 className="text-center">No Books to Display</h1>
-            ) : (
-              <BookList>
-                {books.map(book => {
-                  return (
-                    <BookListItem
-                      key={book.volumeInfo.title}
-                      title={book.volumeInfo.title}
-                      authors={book.volumeInfo.authors}
-                      description={book.volumeInfo.description}
-                      link={book.volumeInfo.infoLink}
-                      thumbnail={book.volumeInfo.imageLinks.thumbnail}
-                    />
-                  );
-                })}
-              </BookList>
-            )}
-          </Col>
-        </Row>
-        </Container>
-      
+      <Switch>
+        <Route exact path="/search" component={Search} />
+        {/* <Route exact path="/saved" component={Saved} /> */}
+        <Route exact path="/" component={Search} />
+        </Switch>
     </div>
+</Router>
   );
 }
 
 export default App;
-
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,25 +1,104 @@
-import React from "react";
-// import "./style.css";
+import React, { useState } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Nav from "../components/Nav";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import API from "../utils/API";
+import { BookList, BookListItem } from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-function Search(props) {
+import { useEffect } from "react";
+// import Search from "./pages/Search";
+// import API from "./utils/API";
+
+// import "./App.css";
+// import axios from "axios";
+
+function Search() {
+
+  const [books, setBooks] = useState([]);
+  const [bookSearch, setBookSearch] = useState("");
+
+  useEffect(()=>{
+    API.searchTerms("Ian Fleming").then(response=>
+      console.log(response.data))
+  },[])
+
+  const handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { value } = event.target;
+    setBookSearch(value);
+  };
+
+  const handleFormSubmit = event => {
+    // When the form is submitted, prevent its default behavior, get recipes update the recipes state
+    event.preventDefault();
+    API.searchTerms(bookSearch)
+      .then(res => setBooks(res.data.items))
+      .catch(err => console.log(err));
+  };
+
+
   return (
-    <form className="search">
-      <div className="form-group">
-        <label htmlFor="language">Search Term:</label>
-        <input
-          value={props.search}
-          onChange={props.handleInputChange}
-          name="term"
-          list="term"
-          type="text"
-          className="form-control"
-          placeholder="Type in a search term to begin"
-          id="term"
-        />
-      </div>
-    </form>
+    <div>
+     <Nav/>
+      <Jumbotron />
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <form>
+              <Container>
+                <Row>
+                  <Col size="xs-9 sm-10">
+                    <Input
+                      name="RecipeSearch"
+                      value={bookSearch}
+                      onChange={handleInputChange}
+                      placeholder="Search For a Book"
+                    />
+                  </Col>
+                  <Col size="xs-3 sm-2">
+                    <Button
+                      onClick={handleFormSubmit}
+                      type="success"
+                      className="input-lg"
+                    >
+                        Search
+                    </Button>
+                  </Col>
+                </Row>
+              </Container>
+            </form>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="xs-12">
+            {!books.length ? (
+              <h1 className="text-center">No Books to Display</h1>
+            ) : (
+              <BookList>
+                {books.map(book => {
+                  return (
+                    <BookListItem
+                      key={book.volumeInfo.title}
+                      title={book.volumeInfo.title}
+                      authors={book.volumeInfo.authors}
+                      description={book.volumeInfo.description}
+                      link={book.volumeInfo.infoLink}
+                      thumbnail={book.volumeInfo.imageLinks.thumbnail}
+                    />
+                  );
+                })}
+              </BookList>
+            )}
+          </Col>
+        </Row>
+        </Container>
+      
+    </div>
   );
 }
 
 export default Search;
+
