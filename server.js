09c2403327c8b3bce4ad6e717d67fe1240a6574a
7J647 <007J647@gmail.com>
2020-11-07 20:05:14 -0500

    set up server.js to connect to MongoDB called googlebooks

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.static("client/build"));
 }
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactrecipes", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
